2f323b2b9f58dc6547c3c9af6899852092932d39
Anaks-CT <anaksthayyil30@gmail.com>
2023-09-19 11:25:23 +0530

    fixed a bug where page number and page size should not be less than 0

--- src/service/todo.service.ts
+++ src/service/todo.service.ts
@@ -1,21 +1,29 @@
 import ErrorResponse from "../error/errorResponse";
-import { checkExistingTodo, createNewTodo, deleteTodo, getTodoByUser, getTodosWithOptions, totalTodos, updateTodo as updateTodoOperation } from "../helper/todoCRUD";
 import {
-  checkExistingUserWithUserId,
-} from "../helper/userCRUD";
+  checkExistingTodo,
+  createNewTodo,
+  deleteTodo,
+  getTodoByUser,
+  getTodosWithOptions,
+  totalTodos,
+  updateTodo as updateTodoOperation,
+} from "../helper/todoCRUD";
+import { checkExistingUserWithUserId } from "../helper/userCRUD";
 
 const invalidId = (user_id: number) => {
-  if(!user_id) throw ErrorResponse.badRequest("Please provide an Id for reference in params")
-}
+  if (!user_id)
+    throw ErrorResponse.badRequest(
+      "Please provide an Id for reference in params"
+    );
+};
 
 export const newTodo = async (
   user_id: number,
   title: string,
   completed: boolean = false
 ) => {
-
   // throwing error if userid is not there
-  invalidId(user_id)
+  invalidId(user_id);
 
   // Check if the user with the given user_id exists
   const userExistsResult = await checkExistingUserWithUserId(user_id);
@@ -35,10 +43,8 @@ export const newTodo = async (
 };
 
 export const getTodo = async (user_id: number) => {
-
-    // throwing error if userid is not there
-    invalidId(user_id)
-
+  // throwing error if userid is not there
+  invalidId(user_id);
 
   // Check if the user with the given user_id exists
   const userExistsResult = await checkExistingUserWithUserId(user_id);
@@ -50,23 +56,23 @@ export const getTodo = async (user_id: number) => {
   // Insert a new todo record associated with the user
 
   const todosResult = await getTodoByUser(user_id);
-  const totaltodo = await totalTodos(user_id)
+  const totaltodo = await totalTodos(user_id);
 
-
-  return {todos: todosResult.rows, totaltodo}; 
+  return { todos: todosResult.rows, totaltodo };
 };
 
-
-export const todoUpdate = async (title: string, completed: boolean, todoId: number) => {
-
-    // throwing error if userid is not there
-    invalidId(todoId)
-
+export const todoUpdate = async (
+  title: string,
+  completed: boolean,
+  todoId: number
+) => {
+  // throwing error if userid is not there
+  invalidId(todoId);
 
   // Check if the todo with the given todoid exists
   const todoExistResult = await checkExistingTodo(todoId);
 
-  // throwing error if the todo doesn't exist 
+  // throwing error if the todo doesn't exist
   if (todoExistResult.rowCount === 0)
     throw ErrorResponse.notFound("Todo not found");
 
@@ -77,15 +83,13 @@ export const todoUpdate = async (title: string, completed: boolean, todoId: numb
 };
 
 export const todoDelete = async (todoId: number) => {
-
-    // throwing error if userid is not there
-    invalidId(todoId)
-
+  // throwing error if userid is not there
+  invalidId(todoId);
 
   // Check if the todo with the given todoid exists
   const todoExistResult = await checkExistingTodo(todoId);
 
-  // throwing error if the todo doesn't exist 
+  // throwing error if the todo doesn't exist
   if (todoExistResult.rowCount === 0)
     throw ErrorResponse.notFound("Todo not found");
 
@@ -95,9 +99,19 @@ export const todoDelete = async (todoId: number) => {
   return todosResult.rows; // Return the deleted  todo
 };
 
+export const todoSearchService = async (
+  userId: number,
+  page: number,
+  pageSize: number,
+  searchQuery: string,
+  filterCompleted?: boolean,
+  sortBy?: "title" | "completed"
+) => {
+    // throwing error if userid is not there
+    invalidId(userId);
 
-export const todoSearchService = async (userId: number, page: number, pageSize: number, searchQuery: string, filterCompleted?: boolean, sortBy?: 'title' | 'completed') => {
-
+    if(page < 1) throw ErrorResponse.badRequest("Page number cannot be less than 1")
+    if(pageSize < 1) throw ErrorResponse.badRequest("Page size cannot be less than 1")
 
   // Check if the user with the given user_id exists
   const userExistsResult = await checkExistingUserWithUserId(userId);
@@ -107,15 +121,21 @@ export const todoSearchService = async (userId: number, page: number, pageSize: 
     throw ErrorResponse.notFound("User not found");
 
 
-// updating todo with the associated id
-const todosResult = await getTodosWithOptions(+userId!, +page!, +pageSize!, searchQuery as string, filterCompleted, sortBy==="title" ? "title" : "completed");
-
+  // updating todo with the associated id
+  const todosResult = await getTodosWithOptions(
+    +userId!,
+    +page!,
+    +pageSize!,
+    searchQuery as string,
+    filterCompleted,
+    sortBy === "title" ? "title" : "completed"
+  );
 
   // You can also retrieve the total count of todos for pagination purposes
 
   const totalCountResult = await totalTodos(userId);
- 
-  const totalPages= Math.ceil(totalCountResult / pageSize)
 
-return {...todosResult}; // Return all the details
+  const totalPages = Math.ceil(totalCountResult / pageSize);
+
+  return { ...todosResult }; // Return all the details
 };
